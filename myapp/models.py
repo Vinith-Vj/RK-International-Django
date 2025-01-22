4acc658f70c4078ebe0301c242f5e3424e84f9b2
Vinith-Vj <vinithvj555@gmail.com>
2025-01-22 17:54:25 +0530

    hdsv

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=250)
+    client_name = models.CharField(max_length=50)
+    value = models.DecimalField(max_digits=10, decimal_places=3)
+    image = models.ImageField(upload_to='projects/')
+
+    def __str__(self):
+        return self.name
